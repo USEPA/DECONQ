9e5247f87011db8ce43cdfa45eaa59ea9668f2d4
DeaganJordan <deagan.jordan@epa.gov>
2024-02-20 15:20:38 -0500

    Uploading updated game

--- Assets/AnswersScript.cs
+++ Assets/AnswersScript.cs
@@ -2,28 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class AnswersScript : MonoBehaviour
 {
-
-    
+    public int position;
     public QuestionsScript questionSource;
     public int pointer;
     public Color correct;
     public Color wrong;
     public Color greyed;
     public AnswersScript[] others;
-    public string[] answer;
-    public bool[] isAnswer;
+    private string[] answer;
+    private bool[] isAnswer;
     private Color original;
     private Button select;
     TMPro.TextMeshProUGUI script;
     Image image;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
+        pointer = -1;
+        var ansRow = position * 2;
+        var dataset = Resources.Load<TextAsset>("Questions");
+        var rows = dataset.text.Split('\n');
+        var data = Regex.Split(rows[ansRow], ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+        answer = data[1..];
+        var data2 = rows[ansRow + 1].Split(',');
+        var corectString = data2[1..];
+
+        for (int i = 0; i < answer.Length; i++)
+        {
+            answer[i] = answer[i].Replace("\"", string.Empty);
+        }
+        var temp = new List<bool>();
+        for (int i = 0; i < answer.Length; i++)
+        {
+            if (corectString[i].Contains("1"))
+            {
+                temp.Add(true);
+            } 
+            else 
+            {
+                temp.Add(false);
+            }
+        }
+        isAnswer = temp.ToArray();
+
         script = GetComponentInChildren<TMPro.TextMeshProUGUI>();
         image = GetComponent<Image>();
         select = GetComponent<Button>();
--- Assets/QuestionsScript.cs
+++ Assets/QuestionsScript.cs
@@ -2,27 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
 
 public class QuestionsScript : MonoBehaviour
 {
 
     public Button next;
     public AnswersScript[] answers;
-    public string[] question;
+    private string[] question;
     public int pointer;
     public bool answered;
     public bool correct;
+    public int numQuestions;
     public PullThePlank plank;
+    private int incorrect;
+    private int allowed_fails;
     TMPro.TextMeshProUGUI script;
     private child subtitle;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        incorrect = 0;
+        allowed_fails = 1;
         pointer = -1;
-        question = new string[5] { "Serious games are primarily designed for which purpose?", "Which mainstream game has been referenced for its unintended role in modeling disease spread, offering insights into epidemiology?",
-            "With the reduction in costs for gaming hardware, which device has specifically gained significant attention for its application in serious gaming due to immersive experiences?", 
-            "What was the name of the modified version of 'Doom' that was used by the United States military for training?", "What is Tim’s favorite food?" };
+        var dataset = Resources.Load<TextAsset>("Questions");
+        var rows = dataset.text.Split('\n');
+        var data = Regex.Split(rows[1], ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+        question = data[1..];
+        numQuestions = question.Length;
+        for (int i = 0; i<numQuestions; i++)
+        {
+            question[i] = question[i].Replace("\"", string.Empty);
+        }
+
+        //question = new string[5] { "Serious games are primarily designed for which purpose?", "Which mainstream game has been referenced for its unintended role in modeling disease spread, offering insights into epidemiology?",
+        //    "With the reduction in costs for gaming hardware, which device has specifically gained significant attention for its application in serious gaming due to immersive experiences?", 
+        //    "What was the name of the modified version of 'Doom' that was used by the United States military for training?", "What is Tim’s favorite food?" };
         script = GetComponent<TMPro.TextMeshProUGUI>();
         subtitle = GetComponentInChildren<child>();
         answered = false;
@@ -33,7 +50,7 @@ public class QuestionsScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pointer>=0 && pointer < 5)
+        if (pointer>=0 && pointer < numQuestions)
         {
             script.text = question[pointer];
         }
@@ -46,12 +63,21 @@ public class QuestionsScript : MonoBehaviour
             subtitle.gameObject.SetActive(false);
             script.alignment = TMPro.TextAlignmentOptions.Center;
         }
-        if (!correct)
+        if (!correct) //Add potential for multiple incorrect
         {
-            plank.PullOut();
-            pointer = 5;
+            incorrect += 1;
+            if (incorrect >= allowed_fails)
+            {
+                plank.PullOut();
+                pointer = numQuestions;
+            }
+            else
+            {
+                //add plank wiggle or something
+            }
+            
         }
-        if (pointer < 4)
+        if (pointer < numQuestions-1)
         {
             pointer += 1;
             answered = false;
